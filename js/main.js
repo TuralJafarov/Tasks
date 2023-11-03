387d85bc38f4c3563fc91e1115063730b811200c
TuralJafarov <tural.cafarov@yahoo.com>
2023-11-03 15:57:31 +0400

    update

--- js/main.js
+++ js/main.js
@@ -110,24 +110,40 @@
 
 
 
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     autoplay:true,
+//     autoplayTimeout:4000,
+//     autoplayHoverPause:false,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:2
+//         },
+//         1000:{
+//             items:3
+//         }
+//     }
+// })
+
+
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
-    autoplay:true,
-    autoplayTimeout:4000,
-    autoplayHoverPause:false,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:2
+            items:3
         },
         1000:{
-            items:3
+            items:1
         }
     }
 })
-
-
